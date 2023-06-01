fb126efa801cd1d8994221b8186da320a72b8c9b
akhil <akhil@gmail.com>
2023-06-01 12:37:36 +0530

    terraform update

--- servers.tf
+++ servers.tf
@@ -1,3 +1,15 @@
+data "aws_ami" "frontend" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+output "aws_ami" {
+  value = data.aws_ami.frontend.image_id
+}
+
+
+
 resource "aws_instance" "frontend" {
   ami           = "ami-08ee87f57b38db5af"
   instance_type = "t3.micro"
