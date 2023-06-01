38d71beddeb5812042d558063452c7fed9e26e55
akhil <akhil@gmail.com>
2023-06-01 12:43:43 +0530

    terraform update

--- servers.tf
+++ servers.tf
@@ -1,24 +1,87 @@
-data "aws_ami" "frontend" {
+data "aws_ami" "centos" {
   owners      = ["973714476881"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
-output "aws_ami" {
-  value = data.aws_ami.frontend.image_id
-}
-
-
-
 resource "aws_instance" "frontend" {
-  ami           = "ami-08ee87f57b38db5af"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
   }
 }
 
-output "aws_instance" {
-  value = aws_instance.frontend.public_ip
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+resource "aws_instance" "redis" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+resource "aws_instance" "user" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+resource "aws_instance" "cart" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+resource "aws_instance" "mysql" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+resource "aws_instance" "shipping" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+resource "aws_instance" "payment" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
+  }
 }
