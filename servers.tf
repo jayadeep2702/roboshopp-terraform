5096a96189fc6463e0618969dc8278771737a9a4
akhil <akhil@gmail.com>
2023-06-01 12:30:12 +0530

    terraform update

--- servers.tf
+++ servers.tf
@@ -1,19 +1,8 @@
-
-resource "aws_instance" "instance" {
-  for_each = var.components
-  ami      = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = each.value["instance_type"]
+resource "aws_instance" "frontend" {
+  ami           = "ami-08ee87f57b38db5af"
+  instance_type = "t3.micro"
 
   tags = {
-    Name = each.value["name"]
+    Name = "HelloWorld"
   }
-}
-
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id = "Z02359642VV81PVIOF0ZQ"
-  name    = "${each.value["name"]}-dev.jdevops72.online"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.instance[each.value["name"]].private_ip]
 }
