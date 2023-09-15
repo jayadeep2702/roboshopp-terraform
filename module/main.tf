e3d6e5a4372bb5eb8cc04647d0026469f0afe555
akhil <akhil@gmail.com>
2023-09-15 13:07:18 +0530

    moving files

---
+++ module/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+
+  tags = {
+    Name = var.components_name
+  }
+}
+
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance,aws_route53_record.records]
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/jayadeep2702/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${var.components_name}.sh  ${var.password}"
+    ]
+  }
+}
+
+
+resource "aws_route53_record" "records" {
+
+  zone_id = "Z05101383TRG2EDZQVJHC"
+  name    = "${var.components_name}-dev.jdevops72.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.instance.private_ip]
+}
--- servers.tf
+++ servers.tf
@@ -1,41 +1,8 @@
-resource "aws_instance" "instance" {
-  for_each      = var.components
-  ami           = data.aws_ami.centos.image_id
+module "servers" {
+  for_each = var.components
+  source = "./module"
+  components_name = each.value["name"]
+  env = var.env
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = each.value["name"]
-  }
-}
-
-resource "null_resource" "provisioner" {
-  depends_on = [aws_instance.instance,aws_route53_record.records]
-  for_each = var.components
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.instance[each.value["name"]].private_ip
-    }
-
-    inline = [
-    "rm -rf roboshop-shell",
-    "git clone https://github.com/jayadeep2702/roboshop-shell.git",
-    "cd roboshop-shell",
-    "sudo bash ${each.value["name"]}.sh  ${lookup(each.value,"password","null")}"
-    ]
-  }
-}
-
-
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id = "Z05101383TRG2EDZQVJHC"
-  name    = "${each.value["name"]}-dev.jdevops72.online"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.instance[each.value["name"]].private_ip]
+  password = lookup(each.value,"password","null")
 }
