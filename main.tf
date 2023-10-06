40411f8a768ce21854fa0a73bbee1f5db4ea4089
akhil <akhil@gmail.com>
2023-10-06 12:35:18 +0530

    moving files

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 module "vpc" {
   source = "git::https://github.com/jayadeep2702/tf-module-vpc.git"
   for_each = var.vpc
+  subnets = each.value["subnets"]
   cidr_block = each.value["cidr_block"]
   tags = local.tags
   env = var.env
+
 
 }
