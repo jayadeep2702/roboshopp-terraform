ae96ccfeb19651ebf60dc3beac8ad7f6fa83846e
akhil kumar <akhil@gmail.com>
2023-10-11 16:38:41 +0530

    moving files

--- main.tf
+++ main.tf
@@ -13,5 +13,5 @@ module "web" {
   source = "git::https://github.com/jayadeep2702/tf-module-app.git"
   for_each = var.app
   instance_type = each.value["instance_type"]
-  subnet_name =element(lookup(lookup(lookup(lookup(module.vpc,"main", null ), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+  subnet_id =element(lookup(lookup(lookup(lookup(module.vpc,"main", null ), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
 }
