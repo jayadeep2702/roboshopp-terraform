eb0224ff034255e063db337ee888f37e12bc2da1
akhil kumar <akhil@gmail.com>
2023-10-14 00:23:20 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -65,3 +65,13 @@ docdb = {
     instance_class = "db.t3.medium"
   }
 }
+
+docdb = {
+  main = {
+    subnet_name = "db"
+    allow_db_cidr = "app"
+    engine_version = "5.7.mysql_aurora.2.11.2"
+    instance_count = 1
+    instance_class = "db.t3.small"
+  }
+}
