849bdc451020c9e0e367c72c6c3800f33cd2a431
akhil kumar <akhil@gmail.com>
2023-10-14 01:14:30 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -75,3 +75,14 @@ docdb = {
     instance_class = "db.t3.small"
   }
 }
+
+elasticache = {
+  main = {
+    subnet_name             = "db"
+    allow_db_cidr           = "app"
+    engine_version          = "6.x"
+    replicas_per_node_group = 1
+    num_node_groups         = 1
+    node_type               = "cache.t3.micro"
+  }
+}
