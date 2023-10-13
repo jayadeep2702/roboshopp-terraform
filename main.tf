3e05069356a739ed89ebc6d3b40648f011a806cb
akhil kumar <akhil@gmail.com>
2023-10-13 15:25:33 +0530

    moving files

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "docdb" {
   source = "git::https://github.com/jayadeep2702/tf-module-docdb.git"
 
   for_each = var.docdb
-  subnets = lookup(lookup(lookup(lookup(module.vpc,"main", null ), "subnets", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc,"main", null ), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
   engine_version = each.value["engine_version"]
 
