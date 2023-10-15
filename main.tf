467a0eb8f86cd7bca299af71d20c92539913da2e
akhil kumar <akhil@gmail.com>
2023-10-15 17:24:19 +0530

    moving files

--- main.tf
+++ main.tf
@@ -99,6 +99,7 @@ module "alb" {
 }
 
 module "app" {
+  depends_on = [module.vpc, module.docdb, module.rds, module.elasticache, module.rabbitmq, module.alb]
   source = "git::https://github.com/jayadeep2702/tf-module-app.git"
   for_each = var.app
   instance_type = each.value["instance_type"]
