9f11285aa784e15b83c1e40c99372bb078dd4275
akhil kumar <akhil@gmail.com>
2023-10-17 13:04:31 +0530

    moving files

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ module "app" {
   vpc_id = lookup(lookup(module.vpc,"main", null ), "vpc_id", null)
   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc,"main", null ), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
   listener_arn = lookup(lookup(module.alb,each.value["lb_type"], null ), "listener_arn", null)
-  listener_arn = lookup(lookup(module.alb,each.value["lb_type"], null ), "dns_name", null)
+  lb_dns_name = lookup(lookup(module.alb,each.value["lb_type"], null ), "dns_name", null)
 
   env = var.env
   bastion_cidr = var.bastion_cidr
