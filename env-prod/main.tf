e4be86ba387c9c48c78fb07c99efdbed28576c8b
akhil kumar <akhil@gmail.com>
2024-06-16 18:08:12 +0530

    moving files

--- env-prod/main.tf
+++ env-prod/main.tf
@@ -13,22 +13,22 @@ vpc              = {
     subnets    = {
       public = {
         name       = "public"
-        cidr_block = ["10.100.0.0/24", "10.100.1.0/24"]
+        cidr_block = ["10.10.0.0/24", "10.10.1.0/24"]
         azs        = ["us-east-1a", "us-east-1b"]
       }
       web = {
         name       = "web"
-        cidr_block = ["10.100.2.0/24", "10.100.3.0/24"]
+        cidr_block = ["10.10.2.0/24", "10.10.3.0/24"]
         azs        = ["us-east-1a", "us-east-1b"]
       }
       app = {
         name       = "app"
-        cidr_block = ["10.100.4.0/24", "10.100.5.0/24"]
+        cidr_block = ["10.10.4.0/24", "10.10.5.0/24"]
         azs        = ["us-east-1a", "us-east-1b"]
       }
       db = {
         name       = "db"
-        cidr_block = ["10.100.6.0/24", "10.100.7.0/24"]
+        cidr_block = ["10.10.6.0/24", "10.10.7.0/24"]
         azs        = ["us-east-1a", "us-east-1b"]
       }
     }
